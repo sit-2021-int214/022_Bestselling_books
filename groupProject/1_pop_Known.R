eb0997ce58e98819c1c02c55c774db4b1e6a54ee
mazrak <68809198+markid1ggwp@users.noreply.github.com>
2021-12-15 15:50:53 +0700

    Add files via upload

---
+++ groupProject/1_pop_Known.R
@@ -0,0 +1,46 @@
+
+dataset <- read_csv("https://raw.githubusercontent.com/sit-2021-int214/022_Bestselling_books/main/groupMid/bestsellers_with_categories_clearing.csv")
+
+View(population_1)
+
+mue0 <- mean(dataset$Price,na.rm = T)
+
+count(population_1)
+
+
+population_1 <- filter(dataset, dataset$Genre == "Fiction")
+
+  n1 = 240
+  mean <- mean(population_1$Price,na.rm = T)
+  mue0  <- 10
+  sd <- sd(population_1$Price,na.rm = T)
+  alpha = 0.05
+  
+  #
+  h0 : mue0 >= 10
+  ha : mue0 < 10
+  
+  #find z
+  z <- (mean - mue0) / (sd/sqrt(n));
+  
+  #find pvalue of z
+  pvalue <- pnorm(1-z);
+
+  #find Critical Value
+  zalpha <- qnorm(0.05);
+  
+  # Using p-value approach
+  if(pvalue<=alpha){
+    print("Reject H0")
+  }else{
+    print("Accept H0")
+  }
+  
+  # Using critical value
+  if(z<=zalpha){
+    print("Reject H0")
+  }else{
+    print("Accept H0")
+  }
+  
+  
---
+++ groupProject/2_pop_known.R
@@ -0,0 +1,57 @@
+dataset <- read_csv("https://raw.githubusercontent.com/sit-2021-int214/022_Bestselling_books/main/groupMid/bestsellers_with_categories_clearing.csv")
+
+count(population_1)
+
+count(population_2)
+
+
+population_1 <- filter(dataset, dataset$Year == 2015)
+
+population_2 <- filter(dataset, dataset$Year == 2016)
+
+
+n1 = 50
+n2 = 50
+mean1 <- mean(population_1$Price,na.rm = T)
+sd1 <- sd(population_1$Price,na.rm = T)
+mean2 <- mean(population_2$Price,na.rm = T)
+sd2 <- sd(population_2$Price,na.rm = T)
+
+alpha = 0.05
+
+#
+h0 : A - B = 0
+ha : A - B != 0
+#find se of mean 
+se <- sqrt((sd1^2)/n1) + ((sd2^2)/n2)
+#find z
+z <- (mean1 - mean2)  / se;
+
+#find pvalue of z
+pvalue <- pnorm(z);
+
+#find Critical Value
+zalpha <- qnorm(1-0.025);
+
+# Using p-value approach
+if(pvalue<=alpha){
+  print("Reject H0")
+}else{
+  print("Accept H0")
+}
+
+# Using critical value
+if(z<=zalpha){
+  print("Reject H0")
+}else{
+  print("Accept H0")
+}
+
+
+#diff mean
+#find margin
+margin <- zalpha*se
+#diff margin
+diffmean <- mean1 - mean2
+upper <- diffmean+margin
+lower <- diffmean-margin
